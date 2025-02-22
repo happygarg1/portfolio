d211c3598860ab08efda776253ef54b12e7c822f
Himani Garg <himanigarg998@gmail.com>
2025-02-23 01:34:51 +0530

    Updated code for deployment

--- components/ui/MovingBorders.tsx
+++ components/ui/MovingBorders.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import {
   motion,
   useAnimationFrame,
@@ -7,43 +7,63 @@ import {
   useMotionValue,
   useTransform,
 } from "framer-motion";
+import { useRef } from "react";
 import { cn } from "@/lib/utils";
 
-type ButtonProps = {
+export function Button({
+  borderRadius = "1.75rem",
+  children,
+  as: Component = "button",
+  containerClassName,
+  borderClassName,
+  duration,
+  className,
+  ...otherProps
+}: {
   borderRadius?: string;
   children: React.ReactNode;
-  as?: React.ElementType;
+  as?: any;
   containerClassName?: string;
   borderClassName?: string;
   duration?: number;
   className?: string;
-} & React.ComponentPropsWithoutRef<"button">; // Allow extra props
-
-export function Button({ as: Component = "button", ...props }: ButtonProps) {
+  [key: string]: any;
+}) {
   return (
     <Component
-      className={cn("bg-transparent relative text-xl md:col-span-2 p-[1px] overflow-hidden", props.containerClassName)}
-      style={{ borderRadius: props.borderRadius }}
-      {...props}
+      className={cn(
+        "bg-transparent relative text-xl md:col-span-2 p-[1px] overflow-hidden ",
+        containerClassName
+      )}
+      style={{
+        borderRadius: borderRadius,
+      }}
+      {...otherProps}
     >
       <div
         className="absolute inset-0"
-        style={{ borderRadius: `calc(${props.borderRadius} * 0.96)` }}
+        style={{ borderRadius: `calc(${borderRadius} * 0.96)` }}
       >
-        <MovingBorder duration={props.duration} rx="30%" ry="30%">
+        <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
-            className={cn("h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]", props.borderClassName)}
+            className={cn(
+              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]",
+              borderClassName
+            )}
           />
         </MovingBorder>
       </div>
+
       <div
         className={cn(
           "relative bg-slate-900/[0.8] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
-          props.className
+          className
         )}
-        style={{ borderRadius: `calc(${props.borderRadius} * 0.96)` }}
+        style={{
+          borderRadius: `calc(${borderRadius} * 0.96)`,
+        }}
       >
-        {props.children}
+        {children}
       </div>
     </Component>
   );
@@ -60,8 +80,9 @@ export const MovingBorder = ({
   duration?: number;
   rx?: string;
   ry?: string;
+  [key: string]: any;
 }) => {
-  const pathRef = useRef<SVGRectElement | null>(null);
+  const pathRef = useRef<any>(null);
   const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
@@ -72,17 +93,45 @@ export const MovingBorder = ({
     }
   });
 
-  const x = useTransform(progress, (val) => pathRef.current?.getPointAtLength(val).x || 0);
-  const y = useTransform(progress, (val) => pathRef.current?.getPointAtLength(val).y || 0);
+  const x = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).x
+  );
+  const y = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).y
+  );
 
   const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
 
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" className="absolute h-full w-full" {...otherProps}>
-        <rect fill="none" width="100%" height="100%" rx={rx} ry={ry} ref={pathRef} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="absolute h-full w-full"
+        width="100%"
+        height="100%"
+        {...otherProps}
+      >
+        <rect
+          fill="none"
+          width="100%"
+          height="100%"
+          rx={rx}
+          ry={ry}
+          ref={pathRef}
+        />
       </svg>
-      <motion.div style={{ position: "absolute", top: 0, left: 0, display: "inline-block", transform }}>
+      <motion.div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "inline-block",
+          transform,
+        }}
+      >
         {children}
       </motion.div>
     </>
